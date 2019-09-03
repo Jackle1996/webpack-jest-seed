32ecddf01fb6f20ebbfced384251a2da593f8195
Yannick Luthiger <yannick.luthiger@stud.hslu.ch>
2019-09-03 10:37:16 +0200

    some commit

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -6,6 +6,29 @@ let techRadar = {
 	  "Techniques": ["Micro Frontends", "Polyglot programming", "Secrets as a service", "Chaos Engineering"],
 	  "Tools": ["Cypress", "Helm", "Traefik", "Humio"],
 	  "Platforms": ["Contentful", "AWS Fargate", "InfluxDB"],
-	  "Languages & Frameworks": ["TypeScript", "Rust"]
+	  "Languages & Frameworks": ["TypeScript", "Rust"],
+	  "Test-FrameWorks": ["Very", "Dynamic", "Coding"]
   }
+};
+// header
+let header = document.createElement("p"); 
+header.innerHTML = techRadar.Meta["generatedAt"];
+document.body.append(header);
+// list
+document.body.append(document.createElement("ul"));
+table = document.body.getElementsByTagName("ul")[0];
+
+for (key in techRadar.TechRadar) {
+	let title = document.createElement("li");
+	title.textContent = key;
+	table.append(title);
+
+	let newUl = document.createElement("ul");
+	table.append(newUl);
+
+	for (attr in techRadar.TechRadar[key]) {
+		let newLi = document.createElement("li");
+		newLi.textContent = techRadar.TechRadar[key][attr];
+		newUl.append(newLi);
+	}
 };
