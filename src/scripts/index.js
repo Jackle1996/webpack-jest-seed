0cc7a9b3dfa1f80b968bdca42127edce1453d241
Yannick Luthiger <yannick.luthiger@stud.hslu.ch>
2019-09-03 12:48:09 +0200

    done

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,10 +1,37 @@
-
 const githubApi = 'https://api.github.com';
 const authorizationHeader = {
-	headers: {'Authorization': 'token <<your token>>'}
+	headers: {'Authorization': '216a742e53bded0685a89b3ea729516f80123e69'}
 };
 
 function search(event, username) {
+	event.preventDefault();
+	getUserData(username).then(data => setUserData(data));
+}
+
+function getUserData(username) {
+	let response = fetch(`${githubApi}/users/${username}`);
+	return response.then(data => { 
+		console.log(data);
+		return data.json();
+	});
+}
+function getFollowers(url){
+	let response = fetch(url);
+	return response.then(data => data.json());
+}
+
+function setUserData(user) {
+	document.getElementById("userImage").src = user["avatar_url"];
+	document.getElementById("username").innerHTML = user["login"];
+	getFollowers(user["followers_url"]).then(data => {
+		data.forEach(element => {
+			console.log(element["login"]);
+			let newLi = document.createElement("li"); 
+			newLi.innerHTML = element["login"];
+			document.getElementById("follower").append(newLi);
+		});
+	});
+	console.log(user["followers_url"]);
 	
 }
 
